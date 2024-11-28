e8832d997ac78db8cfdde58b4a9c6218fbebb04e
shilong0311 <1986510281@qq.com>
2024-11-28 21:23:23 +0800

    调整部分代码,加入嵌入特征csv,嵌入为索引

--- util/utils_specCover.py
+++ util/utils_specCover.py
@@ -11,6 +11,11 @@ def cover2embedings(data_folder, csv_path):
     patient_id = ['Patient ID']
     locations = ['Recording locations:']
     pregnancy_status = ['Pregnancy status']
+    a_set=set()
+    s_set=set()
+    h_set=set()
+    w_set=set()
+    ps_set=set()
 
     patient_files = find_patient_files(data_folder)
     total_spec = ["embedding"]
@@ -24,11 +29,19 @@ def cover2embedings(data_folder, csv_path):
         h = get_height(current_patient_data)
         w = get_weight(current_patient_data)
         ps = get_pregnancy_status(current_patient_data)
+        a_set.add(a)
+        s_set.add(s)
+        h_set.add(h)
+        w_set.add(w)
+        ps_set.add(ps)
+
+        # a_set中某个值的索引
+        # a_set.index(a)
 
         # pregnancy_status.append(ps)
         patient_id.append(pid)
         locations.append(location)
-        total_spec.append([a, s, h, w, ps])
+        total_spec.append([a_set.index(a), s_set.index(s), h_set.index(s), w_set.index(w), ps_set.index(ps)])
 
     info = zip(pid, location, total_spec)
     pd.DataFrame(info).to_csv(f"{csv_path}.csv", index=False, header=False)
